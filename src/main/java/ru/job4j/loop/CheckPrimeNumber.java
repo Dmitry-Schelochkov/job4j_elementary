ae9ee3ea1205216fa4f71f59ee3e0b0f7fe159e3
Dmitry-Schelochkov <delasko@mail.ru>
2026-04-08 21:09:20 +0400

    Простое число (оператор прерывания break) [#505214]

---
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+    public static boolean check(int number) {
+        boolean rsl = number > 1;
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
+    }
+}
